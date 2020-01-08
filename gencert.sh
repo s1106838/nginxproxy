d94d40b2e746f15a453e1630bd85255a0dd36884
s1106838 <47108739+s1106838@users.noreply.github.com>
2020-01-08 15:14:56 +0100

    Update gencert.sh

--- gencert.sh
+++ gencert.sh
@@ -7,7 +7,7 @@
    chmod 400 ca.key
    openssl req -new -x509 -sha256 -days 730 -key ca.key -out ca.crt
    chmod 444 ca.crt
-   openssl genrsa -out sclient-ssl.bauland42.com.key 2048
+   openssl genrsa -out client-sclient-ssl.bauland42.com.key 2048
    chmod 400 client-ssl.bauland42.com.key
    openssl req -new -key client-ssl.bauland42.com.key -sha256 -out client-ssl.bauland42.com.csr
  
